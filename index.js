f7828193157804ec0b78ca4716b1eab51a0142bd
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-03 12:28:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const submitData = (name, email) => {
+
+const object = { 
+    name,
+    email
+  }
+
+const options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  }, 
+  body: JSON.stringify(object)
+};
+
+return fetch('http://localhost:3000/users', options)
+  .then(response => response.json())
+  .then(object => document.body.innerHTML = object["id"])
+  .catch(err => document.body.innerHTML= err.message)
+}
